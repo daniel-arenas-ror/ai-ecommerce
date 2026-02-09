bbec1c35b5eadbed10e39a76af0ae5ea512e4dac
Daniel Arenas <daniel1144151@gmail.com>
2026-02-08 21:19:39 -0500

    add route logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
+
+import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/products/:id" element={<ProductDetail />} />
+
+        <Route path="/login" element={<Login />} />
+        
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<div>Checkout Page</div>} />
+          </Route>
+
+          {/* 404 Page */}
+          <Route path="*" element={<div>Page not found</div>} />
+        </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
