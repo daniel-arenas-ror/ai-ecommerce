663f998e41f1dab98c11b9a0d26975cf169d6c1d
Daniel Arenas <daniel1144151@gmail.com>
2026-03-03 21:51:45 -0500

    update company query

--- src/api/queries/company/index.ts
+++ src/api/queries/company/index.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const GET_COMPANY_MAIN_DATA = gql`
-  query GetCompany {
-    company {
+  query GetCompany($id: ID!) {
+    company(id: $id) {
       id
       title
     }
