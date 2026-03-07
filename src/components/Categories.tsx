17e2587d8de5225bb01db7210536254d707cf494
Daniel Arenas <daniel1144151@gmail.com>
2026-03-07 11:13:00 -0500

    add condition to subcategories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -29,20 +29,22 @@ const Categories: React.FC = () => {
             <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full" />
           </a>
 
-          <div className="invisible absolute left-0 top-full z-50 w-48 translate-y-2 opacity-0 transition-all duration-200 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
-            <ul className="mt-2 border border-gray-100 bg-white py-2 shadow-xl">
-              {category?.subCategories && category?.subCategories?.length > 0 && category?.subCategories.map((sub) => (
-                <li key={sub.id}>
-                  <a
-                    href={`/category/${sub.slug}`}
-                    className="block px-4 py-2 text-sm text-gray-600 transition-colors hover:bg-gray-50 hover:text-black"
-                  >
-                    {sub.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+          { category.subCategories && category.subCategories.length > 0 && (
+            <div className="invisible absolute left-0 top-full z-50 w-48 translate-y-2 opacity-0 transition-all duration-200 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
+              <ul className="mt-2 border border-gray-100 bg-white py-2 shadow-xl">
+                {category?.subCategories && category?.subCategories?.length > 0 && category?.subCategories.map((sub) => (
+                  <li key={sub.id}>
+                    <a
+                      href={`/category/${sub.slug}`}
+                      className="block px-4 py-2 text-sm text-gray-600 transition-colors hover:bg-gray-50 hover:text-black"
+                    >
+                      {sub.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>)
+          }
         </div>
       ))}
     </nav>
