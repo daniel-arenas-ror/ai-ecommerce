ee005c14a4eec63519d8a51e07573f97a53b72e6
Daniel Arenas <daniel1144151@gmail.com>
2026-03-16 22:23:16 -0500

    add empty state to products

--- src/components/ProductDetailComponent/styles/default.tsx
+++ src/components/ProductDetailComponent/styles/default.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import type { ProductDetailCardProps } from '../types';
 
 const DefaultProductDetail: React.FC<ProductDetailCardProps> = ({ product }) => {
+  if(!product){
+    return <div></div>;
+  }
+
   return (
     <div className="bg-white p-10 rounded-[32px] border border-gray-100 shadow-xl">
-      <h2 className="text-2xl font-bold mb-4">The Comfort Runner - Default</h2>
+      <h2 className="text-2xl font-bold mb-4">{product.name}</h2>
       <p className="text-gray-600 leading-relaxed">
-        Introducing our most advanced sneaker yet. Designed for ultimate responsiveness and long-lasting comfort, it features an all-new midsole material and a breathable engineered mesh upper. Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias sequi repellendus perferendis sed.
+        {product.description}
       </p>
     </div>
   );
