acff33971374551202797c10642327cd2df5e23b
Daniel Arenas <daniel1144151@gmail.com>
2026-03-16 22:05:56 -0500

    get company configuration

--- src/types/company.ts
+++ src/types/company.ts
@@ -1,9 +1,11 @@
 import type { CardStyle } from "../components/ProductCard/types"
+import type { ProductDetailStyle } from "../components/ProductDetailComponent/types"
 export interface Company {
   id: number;
   name: string;
   iconUrl: string;
   productCardConfiguration?: CardStyle;
+  productDetailConfiguration?: ProductDetailStyle;
   companyItemConfigurations: [CompanyItemConfigurations]
 }
 
