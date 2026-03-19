65f9af8823703454e90cff99d9aacc4ee735e7dc
Daniel Arenas <daniel1144151@gmail.com>
2026-03-18 21:14:48 -0500

    update product query

--- src/api/queries/product/index.ts
+++ src/api/queries/product/index.ts
@@ -1,26 +1,56 @@
 import { gql } from '@apollo/client';
 
 export const GET_PRODUCT_BY_CATEGORY = gql`
-  query getCategoryProducts($companyId: ID!, $categorySlug: String!, $filter: JSON) {
-    categoryProducts(companyId: $companyId, categorySlug: $categorySlug, filter: $filter) {
+  query getproduct($companyId: ID!, $productSlug: ID!) {
+    product(companyId: $companyId, productSlug: $productSlug) {
       id
       name
       description
       slug
       price
       active
+      __typename
+      categories {
+        id
+        name
+        slug
+      }
       optionValues {
         id
         name
         label
         optionTypeName
       }
-      allImages {
+      variants {
+        id
+        sku
+        price
+        optionValues {
+          id
+          name
+          label
+        }
+        images{
+          id
+          url
+          thumbUrl
+          mediumUrl
+          largeUrl
+        }
+      }
+      images {
         id
         url
         thumbUrl
         mediumUrl
         largeUrl
+      }
+      allImages {
+        id
+        url
+        thumbUrl
+        mediumUrl
+        largeUrl 
       }
     }
   }
