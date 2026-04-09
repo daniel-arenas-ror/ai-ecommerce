cf6e69ecc808c6c39cb9023aed8d72dc2d55aee5
Daniel Arenas <daniel1144151@gmail.com>
2026-04-08 20:41:16 -0500

    update layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header cartLength={0} onCartButtonClick={() => {}} />
-      <main className="mx-auto px-4 sm:px-3 lg:px-4 py-4">
+      <main className="mx-auto sm:px-3 lg:px-4 pt-4">
         <Outlet />
       </main>
 
