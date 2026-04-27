4c5f1ec4ae64aa041d76817d23cbb80cc419bcee
Daniel Arenas <daniel1144151@gmail.com>
2026-04-26 21:04:05 -0500

    add notfound page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="p-20 text-center">
+      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="text-lg">Sorry, the page you're looking for doesn't exist.</p>
+    </div>
+   );
+};
+
+export default NotFoundPage;
